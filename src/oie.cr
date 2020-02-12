899555f4b0fb18284fab4c45d74dd74b4411556b
Christian Kakesa <christian.kakesa@gmail.com>
2020-02-12 17:37:31 +0100

    first commit

---
+++ src/oie.cr
@@ -0,0 +1,92 @@
+require "baked_file_system"
+require "logger"
+require "option_parser"
+require "router"
+
+struct Config
+  property server, port
+
+  def initialize(
+    @server : String = "0.0.0.0",
+    @port : Int32 = 3000
+  )
+  end
+end
+
+CONFIG = Config.new
+
+OptionParser.parse do |opts|
+  opts.on("-s SERVER", "--server SERVER", "define the server host server") do |opt|
+    CONFIG.server = opt
+  end
+  opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
+    CONFIG.port = opt.to_i
+  end
+  opts.invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    STDERR.puts opts
+  end
+end
+
+STDOUT.sync = true
+L = Logger.new(STDOUT)
+L.not_nil!.level = ::Logger::DEBUG if ENV["DEBUG"]?
+
+class Filesystem
+  extend BakedFileSystem
+
+  bake_folder "./static"
+
+  def self.serve(file, context)
+    req = context.request
+    resp = context.response
+    resp.status_code = 200
+    resp.content_type = MIME.from_filename(file.path)
+    resp.set_encoding("UTF-8")
+    if req.headers["Accept-Encoding"]? =~ /gzip/
+      resp.headers["Content-Encoding"] = "gzip"
+      resp.content_length = file.compressed_size
+      file.write_to_io(resp, compressed: true)
+    else
+      resp.content_length = file.size
+      file.write_to_io(resp, compressed: false)
+    end
+    context
+  end
+end
+
+class WebServer
+  include Router
+
+  def routes
+    get "/" do |context, _params|
+      context.response.content_type = "text/html"
+      context.response.print Filesystem.get?("index.html").try(&.gets_to_end)
+      context
+    end
+  end
+
+  def run
+    # Generate virtual filesystem routes from "public" folder
+    Filesystem.files.each do |file|
+      get file.path do |context, _params|
+        Filesystem.serve(file, context)
+      end
+    end
+    routes
+
+    handlers = [
+      HTTP::LogHandler.new(STDOUT),
+      HTTP::ErrorHandler.new,
+      route_handler,
+    ]
+
+    server = HTTP::Server.new(handlers)
+    server.bind_tcp(CONFIG.server, CONFIG.port)
+    L.info "Starting the webapp at: #{CONFIG.server}:#{CONFIG.port}"
+    server.listen
+  end
+end
+
+
+WebServer.new.run
---
+++ src/static/js/lib.js
@@ -0,0 +1,90 @@
+
+var rules = {
+    0: "Départ !",
+    1: "Peux mieux faire :))",
+    2: "Pas mal, mais  <strong>2</strong>",
+    3: "Hi hi hi hi !!!",
+    4: "Qu'est ce que tu croyais ?",
+    5: "Avance de 2 cases",
+    6: "Rejoue",
+    7: "Change ta place avec quelqu'un",
+    8: "Retourne à la case départ",
+    9: "Qu'est ce que t'as cru !",
+    10: "Avance de 2 cases",
+    11: "Salut la famille !",
+    12: "Rejoue",
+    13: "Avance de 3 cases",
+    14: "Va à la case 19",
+    15: "Passe ton tour ;-)))))",
+    16: "Rien du tout",
+    17: "Ok",
+    18: "Rejoue",
+    19: "C'est ouf, il n'y a rien !",
+    20: "Avance de 2 cases",
+    21: "Rien du tout",
+    22: "Tu as eu chaud !",
+    23: "Recule de 2 cases",
+    24: "Rejoue",
+    25: "Go à la case <strong>21</strong>",
+    26: "Passe ton tour ;-)))))",
+    27: "Non mais oh !",
+    28: "Il n'y a rien ici !",
+    29: "Tu y es presque ;))",
+    30: "Arrivée !",
+}
+$(document).ready(function () {
+    // Init
+    $("#rules-desc").text(rules[0])
+
+    // Dice
+    $("#dice-button").on("click", () => {
+        let val = Math.round(Math.random() * 5) + 1
+        return $("#dice").text(val)
+    })
+
+    // Rules
+    $("#rules-plus").on("click", () => {
+        let integer = parseInt($("#step").val(), 10);
+        if (isNaN(integer)) {
+            integer = 0
+        } else if (integer >= 30 || integer < 0) {
+            integer = 0
+        } else {
+            integer = integer + 1
+        }
+        $("#step").val(integer.toString())
+        $("#rules-desc").html(rules[integer])
+    })
+
+    $("#rules-minus").on("click", () => {
+        let integer = parseInt($("#step").val(), 10);
+        if (isNaN(integer)) {
+            integer = 0
+        } else if (integer <= 0 || integer > 30) {
+            integer = 30;
+        } else {
+            integer = integer - 1
+        }
+
+        $("#step").val(integer.toString())
+        $("#rules-desc").html(rules[integer])
+    })
+
+    // Step
+    $("#step").on("keypress", (event) => {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == "13") {
+            event.preventDefault()
+            let integer = parseInt($("#step").val(), 10)
+            if (isNaN(integer)) {
+                integer = 0
+            } else if (integer < 0) {
+                integer = 0
+            } else if (integer > 30) {
+                integer = 30
+            }
+            $("#step").val(integer.toString())
+            $("#rules-desc").html(rules[integer])
+        }
+    });
+});
